e7d2a6e9daf197ff243bfa233e0108049f373455
ugrasK <ugurkaraer1989@gmail.com>
2022-07-26 00:15:43 +0300

    created window and canvas elements and used image file on canvas.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 # ----------------------- CONSTANTS --------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,12 +12,22 @@ LONG_BREAK_MIN = 20
 
 # ----------------------- TIMER RESET --------------------- #
 
-
 # ----------------------- TIMER MECHANISM --------------------- #
-
 
 # ----------------------- COUNTDOWN MECHANISM --------------------- #
 
 
 # ----------------------- UI SETUP --------------------- #
+# create window
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
 
+# create canvas, create variable to save Photoimage, use that in create_image method on canvas
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(102, 112, image=tomato_img)
+canvas.pack()
+
+
+window.mainloop()
