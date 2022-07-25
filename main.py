ee3b84e6441a5190d3eb72c93f7f15296504d6a7
ugrasK <ugurkaraer1989@gmail.com>
2022-07-26 00:42:25 +0300

    created text on image in window. added bg color both window and canvas. refactored code with comments.

--- main.py
+++ main.py
@@ -18,16 +18,23 @@ LONG_BREAK_MIN = 20
 
 
 # ----------------------- UI SETUP --------------------- #
-# create window
+# create a window with Tkinter
 window = Tk()
+# create a title for window
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+# set padding for window on x,y and set a background color
+window.config(padx=100, pady=50, bg=YELLOW)
 
-# create canvas, create variable to save Photoimage, use that in create_image method on canvas
-canvas = Canvas(width=200, height=224)
+# create canvas, set a background color and remove white border with highlightthickness
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# create a Photoimage with file path and save it to a variable
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=tomato_img)
-canvas.pack()
+# use that in create_image method on canvas with pos x,y
+canvas.create_image(100, 112, image=tomato_img)
+# create text with pos x,y , text, fill color and font
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
+# show canvas in window
+canvas.pack()
 
 window.mainloop()
