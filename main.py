7c4e51e056f3781cec7415bd7c2310093e5d5369
ugrasK <ugurkaraer1989@gmail.com>
2022-07-26 01:51:36 +0300

    created a countdown and timer func and used countdown in timer func. add timer func to start button click.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ----------------------- CONSTANTS --------------------- #
 PINK = "#e2979c"
@@ -13,8 +14,19 @@ LONG_BREAK_MIN = 20
 # ----------------------- TIMER RESET --------------------- #
 
 # ----------------------- TIMER MECHANISM --------------------- #
+# create a timer function to use when start button is clicked
+def start_timer():
+    # call countdown function with time input as seconds
+    count_down(300)
 
 # ----------------------- COUNTDOWN MECHANISM --------------------- #
+# create a countdown function to change timer_text
+def count_down(count):
+    count_minute = math.floor(count / 60)
+    count_seconds = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 
 # ----------------------- UI SETUP --------------------- #
@@ -24,6 +36,7 @@ window = Tk()
 window.title("Pomodoro")
 # set padding for window on x,y and set a background color
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 # create a label as Timer and set its position on grid
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
@@ -35,13 +48,13 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 # use that in create_image method on canvas with pos x,y
 canvas.create_image(100, 112, image=tomato_img)
-# create text with pos x,y , text, fill color and font
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+# create text with pos x,y , text, fill color, font and save it in a variable
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 # set canvas position on grid
 canvas.grid(column=1, row=1)
 
-# create a start button and set its position on grid
-start_button = Button(text="Start", highlightthickness=0)
+# create a start button, set its position on grid and use start_timer() function on its command
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 # create a reset button and set its position on grid
 reset_button = Button(text="Reset", highlightthickness=0)
